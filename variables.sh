ef9b67aa296381cea0e908aeeb8aff02b0331b22
kina-mpo <15107701+kina-mpo@users.noreply.github.com>
2022-01-13 12:16:21 +0100

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+# LOCKFILE
+LOCKFILE="/tmp/$(basename $0)"
+
+# COLORS
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+# STATES
+OK="[${GREEN}OK${RESET}]"
+KO="[${RED}KO${RESET}]"
+WARN="[${YELLOW}!!${RESET}]"
+INFO="[${BLUE}..${RESET}]"

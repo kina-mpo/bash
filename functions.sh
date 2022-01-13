be15d7c818b2bdb28a6b3cbb5448a6b4bf98ae46
kina-mpo <15107701+kina-mpo@users.noreply.github.com>
2022-01-13 12:15:01 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+
+function lock {
+	if [ -e $LOCKFILE ] ; then
+		LOCKPID=$(cat $LOCKFILE)
+		if ps $LOCKPID >/dev/null ; then
+			return 1
+		else
+			echo "$$" > $LOCKFILE
+			return 0
+		fi
+	else
+		echo "$$" > $LOCKFILE
+		return 0
+	fi
+}
+
+function unlock {
+	if [ -e $LOCKFILE ] ; then
+		rm $LOCKFILE
+		return 0
+	else
+		return 1
+	fi
+}
+
+function stage {
+	if [ -n $STAGE ] ; then
+		STAGE=$((STAGE+1))
+	else
+		STAGE=1
+	fi
+	if [ $STAGE -lt 10 ] ; then
+		echo -e "[${CYAN}0${STAGE}${RESET}] STAGE - $1"
+	else
+		echo -e "[${CYAN}${STAGE}${RESET}] STAGE - $1"
+	fi
+}
